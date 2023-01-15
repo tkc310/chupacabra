6a91baebe749066a664d021cc3a64a869d586868
Takashi Sato <takashisato.develop@gmail.com>
2023-01-15 17:48:44 +0900

    refactor: remove comments

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,7 @@ import solidPlugin from 'vite-plugin-solid';
 import { manifest as _manifest } from './src/manifest';
 
 const manifest = defineManifest(_manifest);
-
 const root = resolve(__dirname, 'src');
-// const assetsDir = resolve(__dirname, 'assets');
-// const outDir = resolve(__dirname, "dist");
-// const publicDir = resolve(__dirname, "public");
-// const isDev = process.env.__DEV__ === "true";
 
 export default defineConfig({
   plugins: [solidPlugin(), crx({ manifest })],
@@ -19,9 +14,4 @@ export default defineConfig({
       '@': root,
     },
   },
-  // publicDir,
-  // build: {
-  //   outDir,
-  //   sourcemap: isDev,
-  // }
 });
