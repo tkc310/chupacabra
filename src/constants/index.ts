2bbfb7f469f79deef54f157c13e4d56d30adc491
Takashi Sato <takashisato.develop@gmail.com>
2023-01-16 23:21:49 +0900

    fix: forgotted commit

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,9 @@
 export const SERVICE_NAME = 'Chupacabra';
 
+export const SERVICE_KEY = SERVICE_NAME.toLocaleLowerCase();
+
 export const SUPPORTED_ORIGINS = ['https://tkc310.com'];
+export const SUPPORTED_URLS = SUPPORTED_ORIGINS.map((item) => `${item}/*`);
 
 export const CHROME_MESSAGES = {
   EXECUTE: 'EXECUTE',
